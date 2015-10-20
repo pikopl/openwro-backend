3aa353b3c1215c26a08570b1d6fd2e9d9f2afaaa
pikopl <piotr.kopajczyk@gmail.com>
2015-10-20 07:52:59 +0200

    Added String-List of Map conversion for carpark data.

--- core/src/main/java/pl/pikopl/openwro/core/dataconverter/CarParkDataConverter.java
+++ core/src/main/java/pl/pikopl/openwro/core/dataconverter/CarParkDataConverter.java
@@ -3,15 +3,71 @@
  */
 package pl.pikopl.openwro.core.dataconverter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvMapReader;
+import org.supercsv.io.ICsvMapReader;
+import org.supercsv.prefs.CsvPreference;
+
 /**
  * @author kopajczy
  *
  */
 public class CarParkDataConverter {
-	public static void convertCsv(final String data) {
+	
+	public static void convertCsv(final String data) throws IOException {
 		System.out.println("ENTER:> CarParkDataConverter:convertCsv");
-		//TODO: convert csv(string) to object
+		List<Map<String, Object>> csvFileData = new LinkedList<Map<String, Object>>();
+		Reader bufferedReader = new StringReader(data);
+		ICsvMapReader mapReader = new CsvMapReader(bufferedReader, CsvPreference.EXCEL_NORTH_EUROPE_PREFERENCE);
+		String[] csvHeaders;
+		try {
+			// the header columns are used as the keys to the Map
+			csvHeaders = mapReader.getHeader(true);
+			final CellProcessor[] processors = getOptionalProcessorForEveryCol(csvHeaders.length);
+			// load each row to a map
+			Map<String, Object> record;
+			while ((record = mapReader.read(csvHeaders, processors)) != null) {
+				csvFileData.add(record);
+			}
+		} catch(IOException e){
+			System.out.println("ERROR:> CarParkDataConverter:convertCsv: " + e);
+			throw e;
+		}
+		finally {
+			if (mapReader != null) {
+				mapReader.close();
+			}
+		}
 		//TODO: insert converted object to DB
-		System.out.println("EXIT:> CarParkDataConverter:convertCsv");
+		System.out.println("EXIT:> CarParkDataConverter:convertCsv:\n " + csvFileData);
+	}
+	
+	/**
+	 * Cell processors are an integral part of reading and writing with Super
+	 * CSV - they automate the data type conversions, and enforce constraints.
+	 * 
+	 * More about CellProcessors can be found in SuperCSV documentation.
+	 * 
+	 * @param colsNumber
+	 *            number of columns in each row of CSV file
+	 * @return array with cells processors (according to 'colsNumber')
+	 */
+	private static CellProcessor[] getOptionalProcessorForEveryCol(Integer colsNumber) {
+		CellProcessor[] processors = new CellProcessor[colsNumber];
+
+		for (int i = 0; i < processors.length; i++) {
+			processors[i] = new Optional();
+		}
+
+		return processors;
 	}
 }
