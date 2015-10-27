b680d5c199cf8af2e8acc4b5a1c946d23b970012
pikopl <piotr.kopajczyk@gmail.com>
2015-10-27 09:03:16 +0100

    Added SchedulerService to import data from server periodically.

---
+++ core/src/main/java/pl/pikopl/openwro/core/scheduler/SchedulerService.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package pl.pikopl.openwro.core.scheduler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.client.ClientProtocolException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import pl.pikopl.openwro.core.database.DatabaseService;
+import pl.pikopl.openwro.core.dataconverter.CarParkDataConverter;
+import pl.pikopl.openwro.resourceconnector.http.HttpConnector;
+import pl.pikopl.openwro.resourceconnector.http.HttpRequestFailureException;
+
+/**
+ * @author kopajczy
+ *
+ */
+@Service
+public class SchedulerService {
+	
+	@Autowired
+	private DatabaseService dbService;
+	
+	final private static String RESOURCE_URL = "http://www.wroclaw.pl/open-data/opendata/its/parkingi/parkingi.csv";
+	
+	@Scheduled(cron="0 10 * * * ?")
+	public void importData(){
+		System.out.println("ENTER:> " + this.getClass() + ":importData");
+		Long resultCode = -1L;
+		try {
+			final String result = HttpConnector.sendGET(RESOURCE_URL);
+			System.out.println("ANY:> " + this.getClass() + ":importData:result:" + result);
+			List<Map<String, Object>> data = CarParkDataConverter.convertCsv(result);
+			dbService.fillCarkParkData(data);
+			resultCode = 200L;
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (HttpRequestFailureException e) {
+			resultCode = (long) e.getStatusCode();
+			e.printStackTrace();
+		}
+		System.out.println("EXIT:> " + this.getClass() + ":importData:resultCode:" + resultCode);
+	}
+
+}
--- restServices/carparks/src/main/java/pl/pikopl/openwro/carparks/config/SpringRootConfig.java
+++ restServices/carparks/src/main/java/pl/pikopl/openwro/carparks/config/SpringRootConfig.java
@@ -2,14 +2,17 @@ package pl.pikopl.openwro.carparks.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @Configuration
+@EnableScheduling
 @ComponentScan({ 	"pl.pikopl.openwro.carparks.controller", 
 					"pl.pikopl.openwro.dbservice", 
 					"pl.pikopl.openwro.carparks.config",
-					"pl.pikopl.openwro.core.database"})
+					"pl.pikopl.openwro.core.database",
+					"pl.pikopl.openwro.core.scheduler"})
 public class SpringRootConfig {
 	
 }
